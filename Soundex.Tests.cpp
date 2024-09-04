a822f2a0d225464231ddf53ea745919883b8716e
Apoorvakc27 <89474200+Apoorvakc27@users.noreply.github.com>
2024-09-04 09:27:27 +0530

    Update Soundex.Tests.cpp

--- Soundex.Tests.cpp
+++ Soundex.Tests.cpp
@@ -5,5 +5,23 @@ TEST(SoudexTestsuite, ReplacesConsonantsWithAppropriateDigits) {
  //AAA
   char soundex[5];
   generateSoundex("AX", soundex);
- // ASSERT_EQ(soundex,"A200");
+  EXPECT_STREQ(soundex,"A200");
+  //ASSERT_EQ(soundex,"A200");
 }
+TEST(SoudexTestsuite, NumbersWithCharacters) {
+  char soundex[5];
+  generateSoundex("17AK", soundex);
+  EXPECT_STREQ(soundex,"1002");
+  
+}
+
+TEST(SoudexTestsuite, NumbersWithLongCharacters) {
+
+  char soundex[5];
+  generateSoundex("APOO27", soundex);
+  EXPECT_STREQ(soundex,"A100");
+  
+}
+
+
+
