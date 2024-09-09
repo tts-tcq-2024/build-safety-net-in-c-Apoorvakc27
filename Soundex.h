51f33310c8fb25b71d605a39574b53b9e73618ba
Apoorvakc27 <89474200+Apoorvakc27@users.noreply.github.com>
2024-09-09 17:50:50 +0530

    Update Soundex.h

--- Soundex.h
+++ Soundex.h
@@ -11,68 +11,62 @@ char soundexMap[26] = {
     '0', '1', '0', '2', '0', '2'                      // U-Z
 };
 
-/* Supporting function to check if a character is an uppercase letter */
 int isUpperCaseLetter(char c) {
     return c >= 'A' && c <= 'Z';
 }
 
-/* Supporting function to convert character to uppercase */
 char toUpperCase(char c) {
     return toupper(c);
 }
 
-/*  Function to get Soundex code from the lookup table */
 char getSoundexCode(char c) {
     c = toUpperCase(c);
     if (isUpperCaseLetter(c)) {
-        return soundexMap[c - 'A'];  
+        return soundexMap[c - 'A'];  /* It Return's mapped code from lookup table */
     }
-    return '0'; 
+    return '0'; /* For non-alphabet characters returns Zero */
 }
 
-/* Supporting function to check if the code is valid to add */
-int isValidCode(char code, char previousCode) {
+
+int isSuoundxCode(char code, char previousCode) {
     return code != '0' && code != previousCode;
 }
 
-/* Supporting function to add a character to the Soundex code, if valid */
+/* If valid, this helper function to add a character to the Soundex code */
 void addToSoundex(char *soundex, char code, int *sIndex) {
-    if (isValidCode(code, soundex[*sIndex - 1])) {
+    if (isSuoundxCode(code, soundex[*sIndex - 1])) {
         soundex[(*sIndex)++] = code;
     }
 }
 
-/* Supporting function to initialize Soundex code */
 void initializeSoundex(const char *name, char *soundex, int *sIndex) {
     soundex[0] = toUpperCase(name[0]);  
     *sIndex = 1;  /* Initialize the index to the second character */
 }
 
-/* Supporting function to pad remaining characters with '0' */
 void padSoundex(char *soundex, int *sIndex) {
     while (*sIndex < 4) {
         soundex[(*sIndex)++] = '0';
     }
-    soundex[4] = '\0'; 
+    soundex[4] = '\0'; /* If null-terminate the Soundex code */ 
 }
+
 /* Supporting function to check if Soundex code length is sufficient */
 int isSoundexComplete(int sIndex) {
     return sIndex >= 4;
 }
 
-/* Supporting function to check if index is within bounds */
 int isIndexWithinBounds(int i, int len) {
     return i < len;
 }
 
-/* Function to check if conditions for loop continuation are met */
 int canContinueProcessing(const char *name, int i, int len, int *sIndex) {
     return isIndexWithinBounds(i, len) && !isSoundexComplete(*sIndex);
 }
 
 /* Function to process characters and generate Soundex code */
 void processSoundex(const char *name, char *soundex, int *sIndex, int len) {
-    int i = 1;  // Start from the second character of the input
+    int i = 1;  /* Start from the second character of the input */
 
     while (canContinueProcessing(name, i, len, sIndex)) {
         char code = getSoundexCode(name[i]);
